cd280c57a01b624c53bc933940e8e324e2cf225c
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-10-06 12:20:44 -0700

    Add auth for roles

--- auth.py
+++ auth.py
@@ -3,8 +3,20 @@ import time
 
 
 async def check_command_permission(ctx, command):
+    # Get ids of the roles the user has
+    ids = [x.id for x in ctx.author.roles]
+    # Gets the users personal id
     user_id = str(ctx.author.id)
+    # Adds personal id to list
+    ids.append(user_id)
+
+    # Sets up guild db
     guild = str(ctx.guild.id)
     db = DatabaseCommunicator(guild)
-    auth = await db.auth.find_one({"user_id": str(user_id), "access": command})
-    return False if auth is None else True
+    # Checks if any id has permissions
+    for _id in ids:
+        auth = await db.auth.find_one({"user_id": str(_id), "access": command})
+        # If auth returns a value, user has permissions via the _id role
+        if auth is not None:
+            return True
+    return False
